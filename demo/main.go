fe27e800cab74adbf3d5b7965bcf28f06ef17309
Pavel <pavel.paulau@gmail.com>
2016-06-07 20:29:40 -0700

    Show greeting when demo bot starts

--- demo/main.go
+++ demo/main.go
@@ -7,7 +7,18 @@ import (
 
 const (
 	address = "127.0.0.1:8080"
+	greeting = `
+This Telegram chat bot was created for demonstration purposes only.
+
+It receives incoming updates via a webhook with the following address:
+
+	http://127.0.0.1:8080/webhook
+`
 )
+
+func init() {
+	fmt.Println(greeting)
+}
 
 func main() {
 	http.HandleFunc("/webhook", chatBot)

df163b359098713b8cc57a77dd352cd5f4cda334
Pavel <pavel.paulau@gmail.com>
2016-06-07 21:59:04 -0700

    Flatten build hierarchy

--- build.sh
+++ build.sh
@@ -8,10 +8,8 @@ for os in "windows" "linux" "darwin"; do
         ext=".exe"
     fi
 
-    mkdir -p build/${os}
     for arch in "386" "amd64"; do
-        mkdir build/${os}/${arch}
-        GOOS=${os} GOARCH=${arch} go build -a -tags netgo --ldflags '-s -w' -o build/${os}/${arch}/howdy${ext}
-        nrsc build/${os}/${arch}/howdy${ext} app
+        GOOS=${os} GOARCH=${arch} go build -a -tags netgo --ldflags '-s -w' -o build/howdy_${os}_${arch}${ext}
+        nrsc build/howdy_${os}_${arch}${ext} app
     done
 done
